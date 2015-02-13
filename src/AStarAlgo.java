b11f8561dadde2d3026f1e5ad0afbd457ce47cb1
amarzi <amarzi@localhost.localdomain>
2015-02-12 22:19:19 -0500

    added board structure, .gitignore file

---
+++ src/AStarAlgo.java
@@ -0,0 +1,7 @@
+
+public class AStarAlgo {
+	public AStarAlgo() {
+		
+	}
+
+}
---
+++ src/Simulation.java
@@ -0,0 +1,17 @@
+
+public class Simulation {
+	public static void main(String[] args) {
+		String [][] maze1 = { 
+				{"e", "e", "e", "o", "e", "e", "e"},
+				{"e", "o", "e", "e", "o", "e", "e"},
+				{"e", "o", "e", "e", "o", "e", "e"},
+				{"e", "o", "o", "h", "o", "e", "e"},
+				{"e", "e", "o", "e", "e", "e", "e"},
+				{"e", "e", "o", "o", "o", "e", "e"},
+				{"e", "e", "e", "e", "e", "e", "e"},
+		};
+		
+		
+		
+	}
+}
